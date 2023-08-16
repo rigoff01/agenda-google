5979c549c239cd8192802f1e29908deeec3e92d2
rigoff01 <rigoff@gmail.com>
2023-08-16 11:46:53 -0500

    Agenda parte dos

--- funciones.js
+++ funciones.js
@@ -8,3 +8,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1OA4EzPA9nkylD9_LO-R3fzejYPpQDBMb2HErCGEGmtI').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
